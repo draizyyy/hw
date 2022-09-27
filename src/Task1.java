e78dd868edc8b9ab85514a36bea0038e1c0dc4e5
draizyyy <114501893+draizyyy@users.noreply.github.com>
2022-09-27 19:00:25 +0300

    Delete src directory

--- src/Task1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Task1 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.println(n / 100 + (n % 100) / 10 + n % 10 + " " + (n + 2 - (n % 2)));
-    }
-}
